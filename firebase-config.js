8e54d45fb139f26f641a74d578515c71621e375c
guillhermeestefan <guilherme.estefan@conx.com.br>
2026-08-10 18:04:41 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCL0M3YR610pkueIbqFllsvYcv3FNdypGI",
+  authDomain: "painel-residuos.firebaseapp.com",
+  projectId: "painel-residuos",
+  storageBucket: "painel-residuos.firebasestorage.app",
+  messagingSenderId: "406211723737",
+  appId: "1:406211723737:web:2f1cf1b27ad7d6f2ad19c8"
+};
